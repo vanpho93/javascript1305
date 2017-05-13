d5fabe65796521ea6bb38b2852538d613aa5347d
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-13 10:25:47 +0700

    switch

---
+++ c.js
@@ -0,0 +1,16 @@
+const a = 5;
+const b = 6;
+let c;
+let max = console.log ? a : b;
+
+// console.log(1 / 0);
+
+// if (a > b) {
+//     max = a;
+// } else {
+//     max = b;
+// }
+
+//false, 0, undefined, null, NaN
+
+console.log(max);
---
+++ d.js
@@ -0,0 +1,12 @@
+const num = 4;
+
+switch(num) {
+    case 2: 
+        console.log('MONDAY');
+        break;
+    case 3:
+        console.log('TUESDAY');
+        break;
+    default: 
+        console.log('SUNDAY');
+}
