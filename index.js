49c00053bd9a15ef871b85de736d5baa585cb244
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-13 09:30:51 +0700

    cm

---
+++ index.js
@@ -0,0 +1,15 @@
+let a = 5;
+
+// Khai bao hang b
+
+/*
+    Comment something here!!!
+*/
+const b = 10;
+
+console.log(a);
+console.log(b);
+
+// null, boolean, number, string
+// Array, Objects, Functions
+
